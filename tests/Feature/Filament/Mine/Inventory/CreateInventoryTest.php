268b623cb6addd2ca4925eb69e2e0c292c0f8e80
Vladyslav Gladyr <61016858+WyTcorporation@users.noreply.github.com>
2025-09-22 12:18:19 +0300

    Adjust Livewire component test setup

--- tests/Feature/Filament/Mine/Inventory/CreateInventoryTest.php
+++ tests/Feature/Filament/Mine/Inventory/CreateInventoryTest.php
@@ -14,18 +14,16 @@ it('validates unique product and warehouse combination when creating inventory',
 
     $this->actingAs($user);
 
-    $component = null;
+    $component = Livewire::test(CreateInventory::class)
+        ->fillForm([
+            'product_id' => $product->getKey(),
+            'warehouse_id' => $warehouse->getKey(),
+            'qty' => 5,
+            'reserved' => 0,
+        ]);
 
-    expect(function () use (&$component, $product, $warehouse) {
-        $component = Livewire::test(CreateInventory::class)
-            ->fillForm([
-                'product_id' => $product->getKey(),
-                'warehouse_id' => $warehouse->getKey(),
-                'qty' => 5,
-                'reserved' => 0,
-            ])
-            ->call('create');
-    })->not->toThrow(UniqueConstraintViolationException::class);
+    expect(fn () => $component->call('create'))
+        ->not->toThrow(UniqueConstraintViolationException::class);
 
     $component->assertHasErrors(['data.product_id']);
 
