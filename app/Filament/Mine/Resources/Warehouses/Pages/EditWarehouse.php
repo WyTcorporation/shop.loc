0928e264a96a4ebabfaa908f4512362dc2b202bf
Vladyslav Gladyr <61016858+WyTcorporation@users.noreply.github.com>
2025-09-25 09:21:42 +0300

    Fix warehouse form translation dehydration

--- app/Filament/Mine/Resources/Warehouses/Pages/EditWarehouse.php
+++ app/Filament/Mine/Resources/Warehouses/Pages/EditWarehouse.php
@@ -39,4 +39,39 @@ class EditWarehouse extends EditRecord
 
         return $data;
     }
+
+    protected function mutateFormDataBeforeSave(array $data): array
+    {
+        $primaryLocale = config('app.locale');
+
+        if (blank($data['name_translations'][$primaryLocale] ?? null)) {
+            $fallbackName = collect($data['name_translations'] ?? [])
+                ->first(fn ($value) => filled($value));
+
+            if (filled($fallbackName)) {
+                $data['name_translations'][$primaryLocale] = $fallbackName;
+            }
+        }
+
+        if (blank($data['name'] ?? null)) {
+            $data['name'] = $data['name_translations'][$primaryLocale]
+                ?? $this->record?->getRawOriginal('name');
+        }
+
+        if (array_key_exists('description_translations', $data) && blank($data['description_translations'][$primaryLocale] ?? null)) {
+            $fallbackDescription = collect($data['description_translations'] ?? [])
+                ->first(fn ($value) => filled($value));
+
+            if (filled($fallbackDescription)) {
+                $data['description_translations'][$primaryLocale] = $fallbackDescription;
+            }
+        }
+
+        if (blank($data['description'] ?? null)) {
+            $data['description'] = $data['description_translations'][$primaryLocale]
+                ?? $this->record?->getRawOriginal('description');
+        }
+
+        return $data;
+    }
 }
